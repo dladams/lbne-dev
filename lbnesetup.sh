e9cac25bae7a9088f7235f448414cf7b710e466e
David Adams <dladams@bnl.gov>
2014-07-18 15:50:43 -0500

    Moree development. Still not working at FNAL.

--- lbnesetup.sh
+++ lbnesetup.sh
@@ -3,17 +3,20 @@ if test -z "${LBNE_IS_SETUP}"; then
   THISBASE=`basename $LBNE_DEVDIR`
   THISUSER=`whoami`
   source ./config.sh
-  export LBNE_LARPROD=`echo larsoft_${LBNE_LARVERSION}_${LBNE_RQUAL} | sed 's/:/_/g'`
+  export LBNE_LARPROD=`echo larsoft_${LBNE_LARVERSION}_${LBNE_QUAL} | sed 's/:/_/g'`
   export LBNE_GIT_BRANCH_NAME=`echo branch-${THISUSER}-${THISBASE} | sed 's/:/_/g'`
   export LBNE_LINE="=========================================================="
+  echo $LBNE_LINE
+  echo Setting up...
+  echo              Project: $LBNE_PROJECT
   echo LArSoft      version: $LBNE_LARVERSION
   echo            qualifier: $LBNE_QUAL
   echo LArSoft product name: $LBNE_LARPROD
-  echo       Local products: $LOCALPRODS
   echo Branch name: $LBNE_GIT_BRANCH_NAME
   # Setup LArSoft with LBNE code
-  # Eventaully obtain file from environment.
+  # Eventually obtain file from environment.
   # For now hardwire solutions for FNAL and BNL.
+  PRODUCTS=
   SETUPFILE=/grid/fermiapp/lbne/software/setup_lbne.sh
   if [ -r $SETUPFILE ]; then
     source $SETUPFILE
@@ -21,12 +24,16 @@ if test -z "${LBNE_IS_SETUP}"; then
     SETUPFILE=/afs/rhic.bnl.gov/lbne/software/products/setup
     if [ -r $SETUPFILE ]; then
       source $SETUPFILE
+      setup git
+      setup gitflow
+      setup mrb
     else
       SETUPFILE="Not found"
       echo Initial setup file not found!
     fi
   fi
   export LBNE_IS_SETUP=True
+  export MRB_PROJECT=$LBNE_PROJECT
 echo Initial setup file: $SETUPFILE
 else
   echo LBNE is already set up--No action taken.
