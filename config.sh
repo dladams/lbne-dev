e643ac97134390be527fecc46739537d32c3b43c
David Adams <dladams@bnl.gov>
2014-08-28 17:13:13 -0500

    Update to use v02_025_02. Add lbne test (just suggests tests to run)

--- config.sh
+++ config.sh
@@ -10,7 +10,7 @@
 LBNE_PROJECT=larsoft
 
 # For larsoft, specify the version.
-LBNE_LARVERSION=v02_05_00
+LBNE_LARVERSION=v02_05_02
 
 # Specify the UPS qualifier.
 # UPS can be used to discover the available values, e.g.
